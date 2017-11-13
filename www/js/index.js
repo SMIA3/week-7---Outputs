fbb7d762581b54243b93c64f34d3960298bb6957
SMIA3 <smia3_17@uni.worc.ac.uk>
2017-11-13 11:27:43 +0000

    w7
    w

--- www/js/index.js
+++ www/js/index.js
@@ -1,8 +1,13 @@
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+    console.log(navigator.notification);
+}
+
 $(document).on("pagecreate","#pageone",function(){
   $('#submitButton').on("click", function(){
     random();
   });            
-});            
+});
 
 
 function random() {
