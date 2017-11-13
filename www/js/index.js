9d6cf195d969dd78ddd9aa1e0180507a3c099d87
SMIA3 <smia3_17@uni.worc.ac.uk>
2017-11-13 11:39:12 +0000

    wk 7
    7

--- www/js/index.js
+++ www/js/index.js
@@ -1,7 +1,7 @@
-document.addEventListener("deviceready", onDeviceReady, false);
-function onDeviceReady() {
-    console.log(navigator.notification);
-}
+//document.addEventListener("deviceready", onDeviceReady, false);
+//function onDeviceReady() {
+//    console.log(navigator.notification);
+//}
 
 $(document).on("pagecreate","#pageone",function(){
   $('#submitButton').on("click", function(){
@@ -11,12 +11,15 @@ $(document).on("pagecreate","#pageone",function(){
 
 
 function random() {
+    console.log("button Clicked")
     ranState = !Math.round(Math.random());
     if (ranState){
+        console.log("True Clicked")
         navigator.notification.beep(1);
         navigator.vibrate(500);
         alert(ranState) //console.log(ranState);
     } else {
+        console.log("False Clicked")
         navigator.notification.beep(2);
         navigator.vibrate([500, 200, 500]);
         alert(ranState) //console.log(ranState);
