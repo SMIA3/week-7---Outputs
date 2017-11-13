6cfcb685a7e36ddc668a0446b1a3de086cb21f10
SMIA3 <smia3_17@uni.worc.ac.uk>
2017-11-13 10:44:40 +0000

    wk7
    Vibration/Beep disabled

--- www/js/index.js
+++ www/js/index.js
@@ -9,11 +9,11 @@ function random() {
     ranState = !Math.round(Math.random());
     if (ranState){
         alert(ranState);
-        navigator.notification.beep(1);
-        navigator.vibrate(500);
+        //navigator.notification.beep(1);
+        //navigator.vibrate(500);
     } else {
-        navigator.notification.beep(2);
         alert(ranState);
-        navigator.vibrate([500, 200, 500]);
+        //navigator.notification.beep(2);
+        //navigator.vibrate([500, 200, 500]);
     }
 }
